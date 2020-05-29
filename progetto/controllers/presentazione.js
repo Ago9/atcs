60a00d4a6670cf24ed8e63e94056768ace1c75e1
AGO-FISSO <ago9517@gmial.com>
2020-05-29 16:30:01 +0200

    .

--- progetto/controllers/presentazione.js
+++ progetto/controllers/presentazione.js
@@ -1,5 +1,7 @@
 const Presentazione = require('../models/presentazione.js');
 
+
+// pare che basta usare il req.body. nome che dai al form per farlo andare 
 exports.findByUser = (req, res) => {
     console.log(req.params.id);
     Presentazione.findByUser(req.body.visitors, (err, data) => {
